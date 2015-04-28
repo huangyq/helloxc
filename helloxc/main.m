46472769b3fe9176fbdd39725f57cd27068d2075
huangyingqi <411907338@qq.com>
2015-04-28 23:37:41 +0800

    helloone

---
+++ helloxc/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  helloxc
+//
+//  Created by huangyingqi on 15/4/13.
+//  Copyright (c) 2015年 huangyingqi. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
